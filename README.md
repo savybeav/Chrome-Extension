4fb67b008e22d636b5a5fa50197265438b680856
arajput96 <sachem2015@gmail.com>
2021-07-21 23:12:07 -0400

    Day 1 of Hackathon No Progress Sadly

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Chrome-Extension
 Hackathon Chrome Extension Project For Codesmith FTRI 3 Cohort with Savitri Beaver
+
+
+Savitri's Notes:
+
+- use a content script (js file) to inject your script into a page and add it in the manifest file under "content_scripts": [{"matches": ["<all_urls>"],
+"js": ["content.js]}]
+- create a popup html page? 
+- use chrome.tabs.query({currentWindow: true, active: true}, function())
+- make a regex to search for all instances of bootcamp names?
+- background.js file
---
+++ content.js
@@ -0,0 +1,32 @@
+console.log("Hello WOrld")
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Create a bank of reference URLs to compare against
+  const badLinks = ['https://www.fullstackacademy.com', 'fullstackacademy.com'];
+  // store current window.location in a variable
+  let currentPage = window.location.href;
+  console.log(currentPage);
+  // if the current window.location is in our badLinks array
+
+  // if(badLinks.includes(currentPage)){
+  //   // replace that url with codesmith.io
+  //   window.location.replace('https://codesmith.io/');
+  // }
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    modifyUrl, { urls: ['https://www.fullstackacademy.com/'] }, ['blocking']
+  );
+  
+  function modifyUrl(details) {
+    if (details.url === 'https://www.fullstackacademy.com/') {
+      return { redirect: 'https://codesmith.io/' }
+    }
+  }
+
+  // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  //   if(tab.url.indexOf("https://www.fullstackacademy.com/") == 0 && changeInfo.status == "loading") {
+  //       chrome.tabs.update(tabId, {url: "https://codesmith.io/"});
+  //   }
+});
+
+
